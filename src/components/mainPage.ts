17ba3e7403e12008b4a17727087c88b03347ed13
Anastasiya <nastya.mikholap@gmail.com>
2023-02-12 02:38:37 +0100

    refactor: delete comments

--- src/components/mainPage.ts
+++ src/components/mainPage.ts
@@ -1,18 +1,14 @@
-import Cards from './cards';
 import Footer from './footer';
 import Menu from './menu';
 import mainPageDOM from './templates/mainPageDom';
 
 export default class MainPage {
     menu: Menu;
-    // cards: Cards;
     footer: Footer;
 
     constructor() {
         this.menu = new Menu();
-        // this.cards = new Cards();
         this.footer = new Footer();
-
     }
 
     addStructure() {
@@ -25,7 +21,6 @@ export default class MainPage {
     draw() {
         this.addStructure();
         this.menu.drawMenu();
-        // this.cards.draw();
         this.footer.draw();
     }
 }
